46b5340bc912fce469311e12afab97df0a3381b1
Jonas Lalin <1ac6df24@gmail.com>
2019-06-12 09:52:47 +0200

    format code

--- src/test/java/com/acme/reverse_proxy/ReverseProxyMatchingEngineTest.java
+++ src/test/java/com/acme/reverse_proxy/ReverseProxyMatchingEngineTest.java
@@ -1,7 +1,6 @@
 package com.acme.reverse_proxy;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class ReverseProxyMatchingEngineTest {
@@ -9,12 +8,39 @@ public class ReverseProxyMatchingEngineTest {
 
     @Test
     public void testFindBackend() {
-        assertEquals("http://example.com/ failed", "SERVER_A", engine.findBackend("http://example.com/"));
-        assertEquals("http://example.com/helloo failed", "SERVER_A", engine.findBackend("http://example.com/helloo"));
-        assertEquals("http://example.com/hello failed", "SERVER_B", engine.findBackend("http://example.com/hello"));
-        assertEquals("http://example.com/hello/ failed", "SERVER_B", engine.findBackend("http://example.com/hello/"));
-        assertEquals("http://company.com/news failed", "SERVER_G", engine.findBackend("http://company.com/news"));
-        assertEquals("http://company.com/news/it failed", "SERVER_H", engine.findBackend("http://company.com/news/it"));
-        assertEquals("http://google.com/ failed", null, engine.findBackend("http://google.com/"));
+        assertEquals(
+                "http://example.com/ failed",
+                "SERVER_A",
+                engine.findBackend("http://example.com/"));
+
+        assertEquals(
+                "http://example.com/helloo failed",
+                "SERVER_A",
+                engine.findBackend("http://example.com/helloo"));
+
+        assertEquals(
+                "http://example.com/hello failed",
+                "SERVER_B",
+                engine.findBackend("http://example.com/hello"));
+
+        assertEquals(
+                "http://example.com/hello/ failed",
+                "SERVER_B",
+                engine.findBackend("http://example.com/hello/"));
+
+        assertEquals(
+                "http://company.com/news failed",
+                "SERVER_G",
+                engine.findBackend("http://company.com/news"));
+
+        assertEquals(
+                "http://company.com/news/it failed",
+                "SERVER_H",
+                engine.findBackend("http://company.com/news/it"));
+
+        assertEquals(
+                "http://google.com/ failed",
+                null,
+                engine.findBackend("http://google.com/"));
     }
 }
